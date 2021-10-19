1eecf71ca0c571da116e9e2cac2a6c1dfa086d42
Sebastian Rauch <s.rauch94@gmail.com>
2021-10-19 18:02:42 +0200

    Add calls to second library.

--- main.c
+++ main.c
@@ -1,14 +1,14 @@
 #include <flexos/isolation.h>
 #include <flexos/3comptest1/isolated1.h>
-//#include <flexos/3comptest2/isolated2.h>
+#include <flexos/3comptest2/isolated2.h>
 
 #include <flexos/impl/main_annotation.h>
 int main(int __unused argc, char __unused *argv[])
 {
-	uk_pr_info("Hello from main.\n");
+	uk_pr_info("main running in comp %d (%d total)\n", FLEXOS_VMEPT_COMP_ID, FLEXOS_VMEPT_COMP_COUNT);
 
 	flexos_gate(libflexos3comptest1, hello_lib1);
-//	flexos_gate(lib3comptest2, hello_lib2);
+	flexos_gate(libflexos3comptest2, hello_lib2);
 
 	return 0;
 }
