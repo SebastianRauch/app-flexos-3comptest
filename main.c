3254e3ce71d9a50454c2aca74613048f7d9db58e
Sebastian Rauch <s.rauch94@gmail.com>
2021-10-19 14:15:22 +0200

    hange to 2 compartments for now.

--- main.c
+++ main.c
@@ -1,13 +1,14 @@
 #include <flexos/isolation.h>
 #include <flexos/3comptest1/isolated1.h>
-#include <flexos/3comptest2/isolated2.h>
-#include <uk/alloc.h>
-#include <flexos/impl/main_annotation.h>
-#include <stdlib.h>
+//#include <flexos/3comptest2/isolated2.h>
 
+#include <flexos/impl/main_annotation.h>
 int main(int __unused argc, char __unused *argv[])
 {
 	uk_pr_info("Hello from main.\n");
-	
+
+	flexos_gate(lib3comptest1, hello_lib1);
+//	flexos_gate(lib3comptest2, hello_lib2);
+
 	return 0;
 }
